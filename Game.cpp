b298ec0172983db297cde795f7ec327d616cb897
Stanciu <stanciulescu.andrei@yahoo.com>
2020-03-23 20:52:49 +0200

    Chestii de baza

---
+++ Game.cpp
@@ -0,0 +1,36 @@
+#include "headers/Game.h"
+
+Game::Game(int windowWidth, int windowHeight, Uint32 flags) {
+    // Initialize SDL.
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+            return;
+
+    // Create the window where we will draw.
+    window = SDL_CreateWindow("SDL_RenderClear", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, flags);
+
+    // We must call SDL_CreateRenderer in order for draw calls to affect this window.
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    // Select the color for drawing. It is set to black here.
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    // Clear the entire screen to our selected color.
+    SDL_RenderClear(renderer);
+}
+
+void Game::render() {
+    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+    SDL_RenderClear(renderer);
+}
+
+void Game::processEvents() {
+    SDL_PollEvent( event );
+    switch(event->type){
+        case SDL_QUIT:
+            quit = true;
+    }
+
+}
+
+Game::~Game() {
+    SDL_Quit();
+}
